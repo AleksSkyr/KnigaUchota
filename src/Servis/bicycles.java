480055ed852b8026b2a439c1c11553161cdd4acd
AleksSkyr <greshnik1499@gmail.com>
2023-07-02 22:29:56 +0300

    start servis

---
+++ src/Servis/bicycles.java
@@ -0,0 +1,14 @@
+package Servis;
+
+public class bicycles {
+
+    public class Bicycle {
+
+        public String modelName;
+        public int wheelsCount;
+
+        public void updateTyre() {
+            System.out.println("Меняем покрышку");
+        }
+    }
+}
